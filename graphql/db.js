e23dec5f96ed01c664bd273928db168ef74a82d9
Nerdnickk <56470910+Nerdnickk@users.noreply.github.com>
2020-05-04 23:49:49 +0900

    get Movies URL

--- graphql/db.js
+++ graphql/db.js
@@ -1,55 +1,9 @@
-let movies = [
-    {
-        id: 1,
-        name: "Avengers",
-        score: 2
-    },
-    {
-        id: 2,
-        name: "Avengers-fuck you",
-        score: 100
-    },
-    {
-        id: 3,
-        name: "About time",
-        score: 30
-    },
-    {
-        id: 4,
-        name: "Bugs",
-        score: 20
-    },
-    {
-        id: 5,
-        name: "Bug's revenge",
-        score: 100
-    },
-]
+import fetch from "node-fetch";
 
-export const getMovie =() => movies
+const API_URL = "https://yts.mx/api/v2/list_movies.json";
 
-export const getById = id => {
-    const filteredMovies = movies.filter(movie => id === movie.id);
-    return filteredMovies[0]
-}
 
-export const deleteMovie = id => {
-    const cleanedMovie = movies.filter(movie => movie.id !== id);
-    if(movies.length > cleanedMovie.length){
-        movies = cleanedMovie;
-        return true;
-    } else {
-        return false;
-    }
-
-}
-
-export const addMovie = (name, score) => {
-    const newMovie = {
-        id : movies.length +1,
-        name,
-        score
-    }
-    movies.push(newMovie);
-    return newMovie;
-}
+export const getMovies = (limit, rating) => 
+    fetch(`${API_URL}`)
+    .then(res => res.json())
+    .then(json => json.data.movies);
