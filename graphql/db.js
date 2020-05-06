ab536ac536a197871a472d8b74b84fc1a789c77d
Nerdnickk <56470910+Nerdnickk@users.noreply.github.com>
2020-05-06 12:13:12 +0900

    detail suggestions

--- graphql/db.js
+++ graphql/db.js
@@ -1,10 +1,13 @@
 import fetch from "node-fetch";
 
-const API_URL = "https://yts.mx/api/v2/list_movies.json?";
+const API_URL = "https://yts.mx/api/v2/";
+const LIST_MOVIES = `${API_URL}list_movies.json?`;
+const MOVIE_DETAIL = `${API_URL}movie_details.json?`;
+const MOVIE_SUGGESTIONS = `${API_URL}movie_suggestions.json?`;
 
 
 export const getMovies = (limit, rating) => {
-    let REQUEST_URL = API_URL;
+    let REQUEST_URL = LIST_MOVIES;
     if(limit > 0){
         REQUEST_URL += `limit=${limit}`
     }
@@ -15,4 +18,23 @@ export const getMovies = (limit, rating) => {
     .then(res => res.json())
     .then(json => json.data.movies)
 }
-    
+
+export const getMovie = (id) => {
+    let REQUEST_URL = MOVIE_DETAIL;
+    if(id > 0){
+        REQUEST_URL += `movie_id=${id}`
+    }
+    return fetch(REQUEST_URL)
+    .then(res => res.json())
+    .then(json => json.data.movie)
+}
+
+export const suggestionsMovie = (id) => {
+    let REQUEST_URL = MOVIE_SUGGESTIONS;
+    if(id >0){
+        REQUEST_URL += `movie_id=${id}`
+    }
+    return fetch(REQUEST_URL)
+    .then(res => res.json())
+    .then(json => json.data.movies)
+}
